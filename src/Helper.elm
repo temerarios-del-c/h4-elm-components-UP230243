beda96189f247aab07eb50e0e038000788fa0b25
UP230243 <up230243@alumnos.upa.edu.mx>
2026-02-22 17:25:13 -0600

    prueba10

--- src/Helper.elm
+++ src/Helper.elm
@@ -3,34 +3,29 @@ module Helper exposing (evalChars, headers, hyperlink, isUpperChars, joinWords)
 import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
 import Html.Attributes exposing (href)
 
-
 joinWords : String -> String -> String
-joinWords str1 str2 =
-    str1 ++ str2
-
+joinWords word1 word2 =
+    word1 ++ word2
 
 isUpperChars : List Char -> List Bool
-isUpperChars chars =
-    List.map Char.isUpper chars
-
+isUpperChars list =
+    List.map Char.isUpper list
 
 evalChars : List Char -> (Char -> Bool) -> List Bool
-evalChars chars predicate =
-    List.map predicate chars
-
+evalChars list funTrans =
+    List.map funTrans list
 
 headers : String -> Html msg
-headers content =
+headers param =
     div []
-        [ h1 [] [ text content ]
-        , h2 [] [ text content ]
-        , h3 [] [ text content ]
-        , h4 [] [ text content ]
-        , h5 [] [ text content ]
-        , h6 [] [ text content ]
+        [ h1 [] [ text param ]
+        , h2 [] [ text param ]
+        , h3 [] [ text param ]
+        , h4 [] [ text param ]
+        , h5 [] [ text param ]
+        , h6 [] [ text param ]
         ]
 
-
 hyperlink : String -> String -> Html msg
-hyperlink url linkText =
-    a [ href url ] [ text linkText ]
+hyperlink url textContent =
+    a [ href url ] [ text textContent ]
